2d85f8cfaf3a484503d2f1f6cea5017a2623cce1
zilani131 <zilani.cuet@gmail.com>
2022-10-08 11:53:05 +0600

    string summation

---
+++ module_1/2.py
@@ -0,0 +1,7 @@
+from unittest import result
+
+
+x=int(input("Enter the first number "))
+y=int(input("Enter the second number "))
+result =x*y
+print('Result is ',result)
---
+++ module_1/3.py
@@ -0,0 +1,4 @@
+firstName=input("Input first name ")
+lastName =input("Input last name ")
+fullName=firstName+" "+lastName
+print('Full name is ',fullName)
